20a29a942e7b6dbf3be4289bb65dbbad9a012c3b
sam19967 <sammie.barasa@students.jkuat.ac.ke>
2025-07-25 22:44:41 +0300

    fixed minor eerrors preventing and added auto generated emails and other required fields which wont be available  during phone number login but are required

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
   uid: { type: String, required: true, unique: true },
-  email: { type: String, required: true },
+  email: { type: String },
   displayName: { type: String },
   phoneNumber: { type: String },
   photoURL: { type: String },
